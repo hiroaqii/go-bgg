36dd0722705284989018f38d1e8ace6a4c2f691f
hiroaqii <hiroaqii@gmail.com>
2022-11-20 20:41:14 +0900

    add thing

---
+++ examples/thing/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/hiroaqii/bgggo"
+)
+
+func main() {
+	hotItems, err := bgggo.Thing()
+	if err != nil {
+		println(err)
+		return
+	}
+
+	e, _ := json.Marshal(hotItems)
+	fmt.Println(string(e))
+
+}
---
+++ thing.go
@@ -0,0 +1,222 @@
+package bgggo
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+type ThingIntermediate struct {
+	XMLName    xml.Name `xml:"items"`
+	Text       string   `xml:",chardata"`
+	Termsofuse string   `xml:"termsofuse,attr"`
+	Item       []struct {
+		Text      string `xml:",chardata"`
+		Type      string `xml:"type,attr"`
+		ID        string `xml:"id,attr"`
+		Thumbnail string `xml:"thumbnail"`
+		Image     string `xml:"image"`
+		Name      []struct {
+			Text      string `xml:",chardata"`
+			Type      string `xml:"type,attr"`
+			Sortindex string `xml:"sortindex,attr"`
+			Value     string `xml:"value,attr"`
+		} `xml:"name"`
+		Description   string `xml:"description"`
+		Yearpublished struct {
+			Text  string `xml:",chardata"`
+			Value string `xml:"value,attr"`
+		} `xml:"yearpublished"`
+		Minplayers struct {
+			Text  string `xml:",chardata"`
+			Value string `xml:"value,attr"`
+		} `xml:"minplayers"`
+		Maxplayers struct {
+			Text  string `xml:",chardata"`
+			Value string `xml:"value,attr"`
+		} `xml:"maxplayers"`
+		Poll []struct {
+			Text       string `xml:",chardata"`
+			Name       string `xml:"name,attr"`
+			Title      string `xml:"title,attr"`
+			Totalvotes string `xml:"totalvotes,attr"`
+			Results    []struct {
+				Text       string `xml:",chardata"`
+				Numplayers string `xml:"numplayers,attr"`
+				Result     []struct {
+					Text     string `xml:",chardata"`
+					Value    string `xml:"value,attr"`
+					Numvotes string `xml:"numvotes,attr"`
+					Level    string `xml:"level,attr"`
+				} `xml:"result"`
+			} `xml:"results"`
+		} `xml:"poll"`
+		Playingtime struct {
+			Text  string `xml:",chardata"`
+			Value string `xml:"value,attr"`
+		} `xml:"playingtime"`
+		Minplaytime struct {
+			Text  string `xml:",chardata"`
+			Value string `xml:"value,attr"`
+		} `xml:"minplaytime"`
+		Maxplaytime struct {
+			Text  string `xml:",chardata"`
+			Value string `xml:"value,attr"`
+		} `xml:"maxplaytime"`
+		Minage struct {
+			Text  string `xml:",chardata"`
+			Value string `xml:"value,attr"`
+		} `xml:"minage"`
+		Link []struct {
+			Text  string `xml:",chardata"`
+			Type  string `xml:"type,attr"`
+			ID    string `xml:"id,attr"`
+			Value string `xml:"value,attr"`
+		} `xml:"link"`
+	} `xml:"item"`
+}
+
+type ThingItems struct {
+	Items []ThingItem `json:"items"`
+}
+
+type ThingItem struct {
+	ID            int         `json:"id"`
+	Thumbnail     string      `json:"thumbnail,omitempty"`
+	Image         string      `json:"image,omitempty"`
+	Name          string      `json:"name,omitempty"`
+	Description   string      `json:"description,omitempty"`
+	Yearpublished int         `json:"yearpublished"`
+	Minplayers    int         `json:"minplayers,omitempty"`
+	Maxplayers    int         `json:"maxplayers,omitempty"`
+	Minplaytime   int         `json:"minplaytime,omitempty"`
+	Maxplaytime   int         `json:"maxplaytime,omitempty"`
+	Minage        int         `json:"minage,omitempty"`
+	Playingtime   int         `json:"playingtime,omitempty"`
+	Polls         []ThingPool `json:"poll,omitempty"`
+	Links         []ThingLink `json:"link,omitempty"`
+}
+
+type ThingPool struct {
+	Name       string             `json:"name,omitempty"`
+	Title      string             `json:"title,omitempty"`
+	Totalvotes int                `json:"totalvotes"`
+	Results    []ThingPoolResults `json:"results,omitempty"`
+}
+
+type ThingPoolResults struct {
+	Numplayers string                   `json:"numplayers,omitempty"`
+	Result     []ThingPoolResultsResult `json:"result,omitempty"`
+}
+
+type ThingPoolResultsResult struct {
+	Level    int    `json:"level,omitempty"`
+	Value    string `json:"value,omitempty"`
+	Numvotes int    `json:"numvotes"`
+}
+
+type ThingLink struct {
+	ID    int    `json:"id,omitempty"`
+	Type  string `json:"type,omitempty"`
+	Value string `json:"value,omitempty"`
+}
+
+func Thing() (ThingItems, error) {
+	resp, err := http.Get("https://api.geekdo.com/xmlapi2/thing?id=10000,2")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var itemInter ThingIntermediate
+	err = xml.Unmarshal(body, &itemInter)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+	}
+
+	var thingItems = []ThingItem{}
+	for _, item := range itemInter.Item {
+		id, _ := strconv.Atoi(item.ID)
+		yearpublished, _ := strconv.Atoi(item.Yearpublished.Value)
+		minplayers, _ := strconv.Atoi(item.Minplayers.Value)
+		maxplayers, _ := strconv.Atoi(item.Maxplayers.Value)
+		minplaytime, _ := strconv.Atoi(item.Minplaytime.Value)
+		maxplaytime, _ := strconv.Atoi(item.Maxplaytime.Value)
+		minage, _ := strconv.Atoi(item.Minage.Value)
+		playingtime, _ := strconv.Atoi(item.Playingtime.Value)
+
+		name := ""
+		for _, nm := range item.Name {
+			if nm.Type == "primary" {
+				name = nm.Value
+			}
+		}
+
+		var polls = []ThingPool{}
+		for _, pool := range item.Poll {
+
+			var poolResults = []ThingPoolResults{}
+			for _, results := range pool.Results {
+
+				var resultsResult = []ThingPoolResultsResult{}
+				for _, r := range results.Result {
+					level, _ := strconv.Atoi(r.Level)
+					numvotes, _ := strconv.Atoi(r.Numvotes)
+					resultsResult = append(resultsResult, ThingPoolResultsResult{
+						Level:    level,
+						Value:    r.Value,
+						Numvotes: numvotes,
+					})
+				}
+
+				poolResults = append(poolResults, ThingPoolResults{
+					Numplayers: results.Numplayers,
+					Result:     resultsResult,
+				})
+			}
+
+			totalvotes, _ := strconv.Atoi(pool.Totalvotes)
+			polls = append(polls, ThingPool{
+				Name:       pool.Name,
+				Title:      pool.Title,
+				Totalvotes: totalvotes,
+				Results:    poolResults,
+			})
+		}
+
+		var links = []ThingLink{}
+		for _, link := range item.Link {
+			id, _ := strconv.Atoi(link.ID)
+			links = append(links, ThingLink{
+				ID:    id,
+				Type:  link.Type,
+				Value: link.Value,
+			})
+		}
+		thingItems = append(thingItems, ThingItem{
+			ID:   id,
+			Name: name,
+			//Thumbnail:     item.Thumbnail,
+			//Image:         item.Image,
+			//Description:   item.Description,
+			Yearpublished: yearpublished,
+			Minplayers:    minplayers,
+			Maxplayers:    maxplayers,
+			Minplaytime:   minplaytime,
+			Maxplaytime:   maxplaytime,
+			Minage:        minage,
+			Playingtime:   playingtime,
+			Polls:         polls,
+			//Links:         links,
+		})
+	}
+
+	return ThingItems{Items: thingItems}, nil
+}
